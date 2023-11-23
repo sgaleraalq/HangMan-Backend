0a9119416846384fe2105c6f8254882453abf72f
sgaleraalq <sgaleraalq@gmail.com>
2023-11-23 15:46:04 +0100

    sign up testing

--- backend/users/signUp/sign_up.py
+++ backend/users/signUp/sign_up.py
@@ -8,8 +8,17 @@ from users.user import User, UserDB
 
 signup = APIRouter(prefix="/signup")
 
+@signup.get("/",status_code=200)
+async def loop_users(user: User):
+    users_client = client["hangman"]["users"]
+    user_exists = any(user.user_name in existing_user.values() for existing_user in users_client.find())
+    
+    print(user_exists)
+    return 
 
-@signup.post("/", response_model=dict, status_code=201)
+
+
+@signup.post("/", response_model=User, status_code=201)
 async def sign_up(user: User):
     users_client = client["hangman"]["users"]
 
@@ -20,7 +29,12 @@ async def sign_up(user: User):
             detail="User already in the database"
             )
     
-    new_user = {"_id":str(ObjectId()),user.user_name:dict(user)}
-    users_client.insert_one(new_user)
+    documento_sin_id = {
+    "nombre": "Ejemplo",
+    "edad": 25,
+    "email": "ejemplo@example.com"
+    }
 
-    return new_user
+    print(user)
+    users_client.insert_one(documento_sin_id)
+    return user
