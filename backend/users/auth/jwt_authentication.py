a64e3d7f16b1ae14b1eda333b90f4d2533e70d18
sgaleraalq <sgaleraalq@gmail.com>
2023-11-28 13:39:48 +0100

    clean_code

--- backend/users/auth/jwt_authentication.py
+++ backend/users/auth/jwt_authentication.py
@@ -15,9 +15,9 @@ async def auth_user(token: str = Depends(oauth2)):
     exception = HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail = "User not authorized to perform this action", headers={"WWW-Authenticate": "Bearer"})
     try:
         user_name = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM]).get("sub")
-        print(user_name)
         if user_name is None:
             raise exception
+        
     except JWTError:
         raise exception
 
@@ -25,7 +25,6 @@ async def auth_user(token: str = Depends(oauth2)):
 
 
 async def current_user(user: User = Depends(auth_user)):
-    print(user)
     if user.disabled:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user")
     
