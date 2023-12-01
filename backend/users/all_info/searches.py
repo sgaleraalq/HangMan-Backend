053eb9b829adc1ded9aafab74eb0124d2ba1d3eb
sgaleraalq <sgaleraalq@gmail.com>
2023-12-01 14:46:12 +0100

    Change return dictionary

--- backend/users/all_info/searches.py
+++ backend/users/all_info/searches.py
@@ -1,4 +1,5 @@
 
+from fastapi import HTTPException, status
 from schemas.user import user_schema
 from client.connect_client import client
 from users.user import User
@@ -7,4 +8,4 @@ def search_user(field:str, key:str) -> User | dict:
     try: 
         return User(**user_schema(client.hangman.users.find_one({field: key})))
     except:
-        return {"error": "No se ha encontrado el usuario"}
+        return {"detail": "User not found in database"}
