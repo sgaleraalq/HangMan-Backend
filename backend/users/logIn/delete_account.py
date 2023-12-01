6774d76fd4dd6fe17d224ba2e9ec3b5e7567123c
sgaleraalq <sgaleraalq@gmail.com>
2023-12-01 14:44:31 +0100

    User deletion

---
+++ backend/users/logIn/delete_account.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from requests import delete
+
+from users.user import User
+from client.connect_client import client
+from users.auth.jwt_authentication import current_user
+from users.all_info.searches import search_user
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"],
+    responses={404: {"description": "Not found"}}
+)
+
+@router.post("/delete_account", response_model=dict, status_code=status.HTTP_201_CREATED)
+async def auth_user(user: User = Depends(current_user)):
+    # User has to be authenticated to auth his account
+
+    if type(user) == User:
+        client.hangman.users.delete_one({"user_name": user.user_name})
+        return {"detail": "Account deleted successfully"}
+
+    else:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "User not found in database"
+        )
